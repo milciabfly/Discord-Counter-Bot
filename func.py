64cb3e5ce1af324e2bed15d57130f776bcd49033
Milcia <milcia@mail.milhomescd.com>
2025-02-16 02:55:02 +0900

    リセットボタン追加

--- func.py
+++ func.py
@@ -13,3 +13,10 @@ class CounterView(discord.ui.View):
         await interaction.response.edit_message(content=f"現在のカウント数：{g_value.count}", view=CounterView())
         with open(g_path.path, "w") as file:
             json.dump(g_value.count, file, indent=4)
+
+    @discord.ui.button(label="リセットする！", style=discord.ButtonStyle.red, custom_id="counter_reset")
+    async def count_reset(self, interaction: discord.Interaction, button: discord.ui.Button):
+        g_value.count = 0
+        await interaction.response.edit_message(content=f"現在のカウント数：{g_value.count}", view=CounterView())
+        with open(g_path.path, "w") as file:
+            json.dump(g_value.count, file, indent=4)
